3d4e169a162447259e89dfdd416c83127ec8142b
ToaztBurnt <112440131+CarlosFigueroa04@users.noreply.github.com>
2022-10-12 21:40:15 +0000

    Turtle function drawing finished

---
+++ ch05/exercises/drawing.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle = turtle.Turtle()
+
+def drawEQshape(num_sides , side_length):
+  for i in range(num_sides):
+    turtle.forward(side_length)
+    turtle.left(angle)
+  
+
+num_sides = int(input("How many sides are in your shape?"))
+side_length = int(input("How long are the shapes sides?"))
+angle = 360/num_sides
+
+drawEQshape(num_sides, side_length)
